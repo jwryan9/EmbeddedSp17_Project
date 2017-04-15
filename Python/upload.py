ed918cc53e19733314680a29869638f3f72a654d
Jason Ryan <jason.w.ryan9@gmail.com>
2017-04-14 19:31:57 -0500

    Added usernam/password input for login

--- Python/upload.py
+++ Python/upload.py
@@ -1,15 +1,13 @@
 import getpass
 import paramiko
 import os
-import socket
 
-server_address = "c-lnx000.engr.uiowa.edu"
 host = "c-lnx000.engr.uiowa.edu"
 server_port = 22
 
 server_root_adr = "/user/j/jwryan/WINDOWS_H_DRIVE/public_html/Embedded/project/"
 
-"""
+
 # Getting the users Eningeering username and ID for transferring the files later in the script
 username = raw_input("\nEnter engineering-ID: ")
 password = getpass.getpass("Enter engineering password: ")
@@ -20,9 +18,6 @@ while password != confirm_pass:
     password = getpass.getpass("Enter engineering password: ")
     confirm_pass = getpass.getpass("Re-enter engineering password: ")
 print ""
-"""
-username = "jwryan"
-password = "jr4ia3908"
 
 print "Uploading file"
 transport = paramiko.Transport(host, server_port)
