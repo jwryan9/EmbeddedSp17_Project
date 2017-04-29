2d799a6509f0d688d10060ea7990af42843d2fa0
Jason Ryan <jason.w.ryan9@gmail.com>
2017-04-29 14:13:37 -0500

    Added AM/PM time designation for uploaded file.

--- Python/generateScoreData.py
+++ Python/generateScoreData.py
@@ -13,7 +13,7 @@ def generate_score_data():
     games = mlbgame.games(year, month, day)
     comb_games = mlbgame.combine_games(games)
 
-    # Write output file (format: away name, home name, inning, away score, home score, start time)
+    # Write output file (format: away name, home name, inning, away score, home score, start time, AM/PM)
     # Inning format (game in progress: current inning, game complete: -1, game to be started: 0)
     with open('../Data/gameData.csv', 'wb') as csv_file:
         out_writer = csv.writer(csv_file)
@@ -22,10 +22,12 @@ def generate_score_data():
 
             if overview['status'] == 'Preview' or overview['status'] == 'Pre-Game':
                 out_writer.writerow([overview['away_team_name'], overview['home_team_name'], '0', '0', '0',
-                                     overview['home_time']])
+                                     overview['home_time'], overview['home_ampm']])
             elif overview['status'] == 'Final':
                 out_writer.writerow([overview['away_team_name'], overview['home_team_name'], '-1',
-                                     overview['away_team_score'], overview['home_team_score'], overview['home_time']])
+                                     overview['away_team_score'], overview['home_team_score'], overview['home_time'],
+                                     overview['home_ampm']])
             elif overview['status'] == 'In Progress':
                 out_writer.writerow([overview['away_team_name'], overview['home_team_name'], overview['inning'],
-                                     overview['away_team_runs'], overview['home_team_runs'], overview['home_time']])
+                                     overview['away_team_runs'], overview['home_team_runs'], overview['home_time'],
+                                     overview['home_ampm']])
